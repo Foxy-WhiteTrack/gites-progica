042ee9b64c6cb894dd48363f34044bb23a8fde72
Foxy-WhiteTrack <48949675+Foxy-WhiteTrack@users.noreply.github.com>
2022-06-08 18:21:25 +0200

    ECF pour l'afpa (part2)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Gite;
+
+use Doctrine\Persistence\ManagerRegistry;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,12 +16,30 @@ class HomeController extends AbstractController
      * @Route("/", name="home")
      */
 
-    public function home()
+    public function home(ManagerRegistry $doctrine)
     {
+        $repository = $doctrine->getRepository(Gite::class);
+        $gites = $repository->findAll();
+
+        // $manager = $doctrine->getManager();
+
+        // $gite = new Gite();
+        // $gite
+        //     ->setNom("Mon premier gite")
+        //     ->setDescription("Lorem")
+        //     ->setSurface(80)
+        //     ->setChambre(3)
+        //     ->setCouchage(5);
+
+        // $manager->persist($gite);
+
+        // $manager->flush();
+
         return $this->render('home/index.html.twig', [
             "titre" => "Super titre",
             "message" => "Helloooooow",
-            "menu" => "home"
+            "menu" => "home",
+            "gites" => $gites
         ]);
     }
 
