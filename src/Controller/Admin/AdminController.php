b4a4adde1f8db07f6efe9fc8d6cc88d0bd91abb9
Foxy-WhiteTrack <48949675+Foxy-WhiteTrack@users.noreply.github.com>
2022-06-09 13:17:02 +0200

    Ajout suppr et token
    Ajout de la méthode suppression et du token de vérification

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -41,11 +41,51 @@ class AdminController extends AbstractController
             $em = $doctrine->getManager();
             $em->persist($gite);
             $em->flush();
+            $this->addFlash('success', 'Gîte ajouté avec succès');
             return $this->redirectToRoute("admin_index");
         }
 
         return $this->renderForm("/admin/gite/create.html.twig", [
             "formGite" => $form
         ]);
+    }
+
+
+    /**
+     * @Route("/admin/gite/edit{id}", name="admin_gite_edit")
+     */
+    public function edit(Gite $gite, Request $request, ManagerRegistry $doctrine)
+    {
+        $form = $this->createForm(GiteType::class, $gite);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em = $doctrine->getManager();
+            $em->flush();
+            $this->addFlash('success', 'Gîte modifié avec succès');
+            return $this->redirectToRoute("admin_index");
+        }
+
+        return $this->renderForm("/admin/gite/edit.html.twig", [
+            "formGite" => $form
+        ]);
+    }
+
+
+    /**
+     * @Route("/admin/gite/delete{id}", name="admin_gite_delete")
+     */
+    public function delete(Gite $gite, ManagerRegistry $doctrine, Request $request)
+    {
+        if ($this->isCsrfTokenValid("gite_delete" . $gite->getId(), $request->request->get('token'))) {
+            $em = $doctrine->getManager();
+            $em->remove($gite);
+            $em->flush();
+            $this->addFlash('success', 'Le gite a bien été supprimé');
+        } else {
+            $this->addFlash('error', 'Token non valide');
+        }
+        return $this->redirectToRoute('admin_index');
     }
 }
