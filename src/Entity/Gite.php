c3fcd1ea03cf7db2296541a24ae9c59b68e06666
Foxy-WhiteTrack <48949675+Foxy-WhiteTrack@users.noreply.github.com>
2022-06-14 20:03:24 +0200

    recherche DQL 2
    recherche DQL avec les contraintes (bugfix)

--- src/Entity/Gite.php
+++ src/Entity/Gite.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\GiteRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=GiteRepository::class)
